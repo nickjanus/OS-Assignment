14530603a5959d9fc0c640181cece5a8908ddc27
Nick Janus <nickjanus@gmail.com>
2014-09-02 00:39:35 +0000

    Adds solution for Q2

--- kernel.c
+++ kernel.c
@@ -1,4 +1,24 @@
+#define COUNT(x) (sizeof(x) / sizeof(x[0])) 
+
+void putChar(long colAddr, long rowAddr, char character);
+
 void main()
 {
-  while (1);
+	putsChar(0xB000, 0x8140, "Hello World")
+	while (1);
+}
+
+void putChar(long colAddr, long rowAddr, char character)
+{
+	putInMemory(colAddr, rowAddr, character); 
+	putInMemory(colAddr, (rowAddr + 0x1), 0x7); 
+}
+
+void putChars(long colAddr, long rowAddr, char chars[])
+{
+	int i;
+	for(i = 0; i < COUNT(chars); i++)
+	{
+		putChar((colAddr + i * 0xA0),rowAddr,chars[i]);
+	}
 }
