236e156cab874277a16b752c77c089a6a614c6ef
Nick Janus <nickjanus@gmail.com>
2014-09-02 01:28:22 +0000

    Adds proof of concept

--- kernel.c
+++ kernel.c
@@ -1,24 +1,6 @@
-#define COUNT(x) (sizeof(x) / sizeof(x[0])) 
-
-void putChar(long colAddr, long rowAddr, char character);
-
 void main()
 {
-	putsChar(0xB000, 0x8140, "Hello World")
+	putInMemory(0xB000, 0x8140, 'A'); 
+	putInMemory(0xB000, 0x8141, 0x7); 
 	while (1);
-}
-
-void putChar(long colAddr, long rowAddr, char character)
-{
-	putInMemory(colAddr, rowAddr, character); 
-	putInMemory(colAddr, (rowAddr + 0x1), 0x7); 
-}
-
-void putChars(long colAddr, long rowAddr, char chars[])
-{
-	int i;
-	for(i = 0; i < COUNT(chars); i++)
-	{
-		putChar((colAddr + i * 0xA0),rowAddr,chars[i]);
-	}
 }
