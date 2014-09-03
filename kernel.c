cd4a7f055e6af2bbacecd8a22d2ec6d34f4782a2
Nick Janus <nickjanus@gmail.com>
2014-09-03 00:50:16 +0000

    Adds solution for Q3

--- kernel.c
+++ kernel.c
@@ -1,29 +1,50 @@
+void printChar(char c);
+void printString(char* str);
+void readString(char buffer[]); 
+
 void main()
 {
-	putInMemory(0xB000, 0x8000, 'H'); 
-	putInMemory(0xB000, 0x8001, 0x7); 
-	putInMemory(0xB000, 0x8002, 'e'); 
-	putInMemory(0xB000, 0x8003, 0x7); 
-	putInMemory(0xB000, 0x8004, 'l'); 
-	putInMemory(0xB000, 0x8005, 0x7); 
-	putInMemory(0xB000, 0x8006, 'l'); 
-	putInMemory(0xB000, 0x8007, 0x7); 
-	putInMemory(0xB000, 0x8008, 'o'); 
-	putInMemory(0xB000, 0x8009, 0x7); 
-	putInMemory(0xB000, 0x800A, ' '); 
-	putInMemory(0xB000, 0x800B, 0x7); 
-	putInMemory(0xB000, 0x800C, 'W'); 
-	putInMemory(0xB000, 0x800D, 0x7); 
-	putInMemory(0xB000, 0x800E, 'o'); 
-	putInMemory(0xB000, 0x800F, 0x7); 
-	putInMemory(0xB000, 0x8010, 'r'); 
-	putInMemory(0xB000, 0x8011, 0x7); 
-	putInMemory(0xB000, 0x8012, 'l'); 
-	putInMemory(0xB000, 0x8013, 0x7); 
-	putInMemory(0xB000, 0x8014, 'd'); 
-	putInMemory(0xB000, 0x8015, 0x7); 
-	putInMemory(0xB000, 0x8016, ' '); 
-	putInMemory(0xB000, 0x8017, 0x7); 
-
+	char line[80]; 
+	printString("Enter a line: "); 
+	readString(line); 
+	printString(line);
 	while (1);
+}
+
+void printChar(char c)
+{	
+	interrupt(0x10, 0xe*256 + c, 0, 0, 0); 
+}
+
+void printString(char* str)
+{
+	while (*str != '\0')
+	{
+		printChar(*str);
+		str++;
+	}
+}
+
+void readString(char buffer[])
+{
+	int i;
+	char letter;
+
+	for (i = 0; i < 79; i++)
+	{
+		letter = interrupt(0x16, 0, 0, 0, 0); 
+		if ((letter == 0x8) && (i > 0)) {
+			i--;	
+		} else if (letter == 0xd) {
+			buffer[i] = 0xa;
+			buffer[i+1] = 0x0;
+			printChar(0xa);
+			printChar(letter);
+			return; 
+		} else {
+			buffer[i] = letter;
+		}
+		
+		printChar(letter);
+	}
 }
